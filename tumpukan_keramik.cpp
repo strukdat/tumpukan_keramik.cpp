5a10b18f5439b8e3920c9a1206750ffd7a747d12
nianuratika <48706479+nianuratika@users.noreply.github.com>
2019-12-10 21:12:49 +0700

    Update tumpukan_keramik.cpp

--- tumpukan_keramik.cpp
+++ tumpukan_keramik.cpp
@@ -7,4 +7,31 @@ using namespace std;
 main(void){
   int pil;
   string data;
-  awal()
+  awal();
+  
+  do{
+    cout<<endl<<endl<<"PROGRAM TUMPUKAN KERAMIK"<<endl;
+        cout<<"----------------------"<<endl;
+        cout << "\n1. Input\n2. Hapus\n3. Tampil\n4. Bersihkan\n5. Keluar\nMasukan pilihan : ";
+        cin >> pil;
+    switch (pil){
+            case 1:
+                cout << "Masukan nama Keramik = "; cin >> data;
+                input (data);
+                break;
+            case 2:
+                hapus();
+                break;
+            case 3:
+                tampil();
+                break;
+            case 4:
+                bersih();
+                break;
+            case 5:
+                cout << "Terimakasih, tekan enter untuk keluar";
+        }
+        getch();
+    }
+    while (pil != 5);
+}
